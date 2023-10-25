4460ece78df7958ea9ba5001d8efbf2756743add
marlonoloo <95957350+marlonoloo@users.noreply.github.com>
2023-10-25 22:41:09 +0300

    Initial commit

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ config = {
     'ssl_ca': 'app/credentials/DigiCertGlobalRootCA.crt.pem',
 }
 
-db_uri = f"mysql+mysqlconnector://{config['user']}:{config['password']}@{config['host']}/{config['database']}?ssl_ca={config['ssl_ca']}"
+db_uri = f'''mysql+mysqlconnector://{config['user']}:{config['password']}@
+{config['host']}/{config['database']}?ssl_ca={config['ssl_ca']}'''

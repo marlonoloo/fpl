024b3a233be5bff211cc701ff7025d7373e67557
marlonoloo <95957350+marlonoloo@users.noreply.github.com>
2023-10-27 14:16:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 config = {
-    'user': 'marlonoloo',
-    'password': 'm4r10n!#',
-    'host': 'fpl.mysql.database.azure.com',
-    'database': 'fpl',
-    'ssl_ca': 'app/credentials/DigiCertGlobalRootCA.crt.pem',
+    'user': '',
+    'password': '',
+    'host': '',
+    'database': '',
+    'ssl_ca': '',
 }
 
 db_uri = f'''mysql+mysqlconnector://{config['user']}:{config['password']}@
